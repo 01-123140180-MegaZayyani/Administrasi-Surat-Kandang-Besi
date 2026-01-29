4b6536e6c65ac9ce6c7623179dc24235800180bb
01-123140180-MegaZayyani <megazayyani115@gmail.com>
2026-01-30 03:40:28 +0700

    update path db

--- backend-desa/server.js
+++ backend-desa/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const prisma = require(".db");
+const prisma = require("./db");
 const cors = require("cors");
 const multer = require("multer");
-
 
 const app = express();
 
