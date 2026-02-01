88dff0e692ab12d39c0c40ece08a5923893d4dc3
01-123140180-MegaZayyani <megazayyani115@gmail.com>
2026-02-02 06:39:06 +0700

    update sevice key

--- backend-desa/src/controllers/middleware/multerSupabase.js
+++ backend-desa/src/controllers/middleware/multerSupabase.js
@@ -4,7 +4,7 @@ const { createClient } = require('@supabase/supabase-js');
 // Inisialisasi Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL, 
-  process.env.SUPABASE_ANON_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 // Gunakan Memory Storage
@@ -16,6 +16,6 @@ const upload = multer({
   }
 });
 
-// Kita export 'upload' (untuk middleware di route) 
+// export 'upload' (untuk middleware di route) 
 // dan 'supabase' (untuk dipakai di controller nanti)
 module.exports = { upload, supabase };
